203af155a0474581ea2614794de26dc1a3ea0044
金文武 <KingTortoise@bogon>
2017-03-06 09:57:56 +0800

    Initial Commit

---
+++ SuitcaseWizard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuitcaseWizard
+//
+//  Created by 金文武 on 17/3/6.
+//  Copyright © 2017年 金文武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
